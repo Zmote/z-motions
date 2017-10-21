771b31787e065f01c91277db257a94ba43879ee6
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2017-10-21 06:28:05 +0200

    jquery and bootstrap setup with basic nav component

---
+++ src/navigation/navigation.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Zmote on 21.10.2017.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-main',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavComponent {
+  Action = 'Zafer is Awesome';
+}
