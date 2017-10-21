cc5dc25a8601ac62d7be41754afe11d443d588b5
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2017-10-21 18:35:09 +0200

    added basic navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {NavComponent} from '../navigation/navigation.component';
+import {FooterComponent} from '../footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/footer/footer.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Zmote on 21.10.2017.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-footer-main',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+}) export class FooterComponent {}
