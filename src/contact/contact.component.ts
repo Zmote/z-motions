06606ce6b747bc3c6509ece3106a8e8909bb4a6a
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2017-10-27 06:16:30 +0200

    changed contact page

--- src/contact/contact.component.ts
+++ src/contact/contact.component.ts
@@ -3,10 +3,27 @@
  */
 
 import {Component} from '@angular/core';
+declare let $: any;
+declare let jQuery: any;
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
-}) export class ContactComponent {
+})
+export class ContactComponent {
+  sendMail() {
+    const form = $('#contact-form').serialize();
+    $.ajax({
+      url: 'http://localhost:3000/sendMail',
+      data: form,
+      success: function (data) {
+        console.log(data);
+      },
+      error: function (data) {
+        console.log(data);
+      }
+    });
+  }
 }
+
